bcaf8e5d292514dd0162a6f34eda759943feb6e5
coeym <janemahia98@gmail.com>
2023-07-13 15:10:11 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
 #include <stistd.h>
 /**
  * main - A c program that prints a line to the standard error
- * Return: 1 (success)
+ * Return: 0 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful" - Dora Korpar, 2015-10-19,\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
