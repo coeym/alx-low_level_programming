50895a53cc65d48a7f73f32c4f255c45b44e0ebe
coeym <janemahia98@gmail.com>
2023-07-13 12:49:56 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Put function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("size of a char: %d byte(s)\n", sizeof(char));
+printf("size of an int: %d byte(s)\n", sizeof(int));
+printf("size of a long int: %d byte(s)\n", sizeof(longint));
+printf("size of a long long int: %d byte(s)\n", sizeof(longlongint));
+printf("size of a float: %d byte(s)\n", sizeof(float));
+return (0);
+}
