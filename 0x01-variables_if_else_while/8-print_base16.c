29afa963cd25d5105ba607f3318c1da0dd50146d
coeym <janemahia98@gmail.com>
2023-07-14 10:46:55 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+int d;
+char c;
+d = 0;
+c = 'a';
+while
+(d < 10)
+{
+putchar(d + '0');
+d++;
+}
+while
+(c <= 'f')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
