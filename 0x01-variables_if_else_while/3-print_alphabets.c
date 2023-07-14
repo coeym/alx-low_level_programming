6d43ee72f22645e14be96cba950bc22e11eae02e
coeym <janemahia98@gmail.com>
2023-07-14 09:02:58 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+char c;
+c = 'A';
+while
+(c <= 'Z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+	return (0);
+}
+
