ddfb5e93610ca8ba7af312ec11649dc7410e61a4
coeym <janemahia98@gmail.com>
2023-07-14 10:18:20 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+int d;
+d = 0;
+while
+(d < 10)
+{
+putchar(d + '0');
+d++;
+}
+putchar('\n');
+return (0);
+}
