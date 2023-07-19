5649a0fbce47ee0f46de0fb0cceeda1374f7e85e
coeym <janemahia98@gmail.com>
2023-07-18 17:46:35 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the value of an integer
+ *@c: The number to be imputed
+ * Return: value of zero
+ */
+int _abs(int c)
+{
+if (c < 0)
+{
+int abs_val;
+abs_val = c * -1;
+return (abs_val);
+}
+return (c);
+}
