618c215fe5fcbd6d023fd68e2d819004cb52f9ec
coeym <janemahia98@gmail.com>
2023-07-24 11:14:09 -0700

    8-print_array

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string
+ * @str: The string to be treated
+ * Return: Always 0
+ */
+
+void puts2(char *str)
+
+{
+
+int x;
+int y = 0;
+
+while (str[y] != '\0')
+{
+y++;
+}
+
+for (x = 0; x < y; x += 2)
+{
+_putchar(str[x]);
+}
+_putchar('\n');
+}
