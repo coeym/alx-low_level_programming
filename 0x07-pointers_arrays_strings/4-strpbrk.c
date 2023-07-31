a296e1223064bc53406d084937ed282836e215ab
coeym <janemahia98@gmail.com>
2023-07-31 02:13:21 -0700

    4-strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
- * _strpbrk - function that searches 
+ * _strpbrk - function that searches
  * a string for any of a set of bytes.
  * @s: input
  * @accept: input
- * Return: Always 0 
+ * Return: Always 0
  */
 
 char *_strpbrk(char *s, char *accept)
