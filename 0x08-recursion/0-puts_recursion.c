4f190370091cdd50932f93b3319cf3608cd967ae
coeym <janemahia98@gmail.com>
2023-08-02 12:05:19 -0700

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,13 +7,15 @@
  * Return: Always 0
  */
 void _putchar(char s);
-void _puts_recursion(char *s){
-if (*s)
+void _puts_recursion(char *s)
 {
-s++;
-_puts_recursion(s);
+if (*s == '\0')
+{
+_putchar('\n');
+return;
 }
 
-else
-_putchar('\n');
+_putchar(*s);
+s++;
+_puts_recursion(s);
 }
