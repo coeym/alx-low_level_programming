a9324f8ce91c57d1283cf96836494337d5675b7a
coeym <janemahia98@gmail.com>
2023-08-10 16:48:00 -0700

    function that allocates memory for an array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,31 +1,44 @@
 #include <stdlib.h>
+#include "main.h"
 
 /**
-  * _calloc - allocates memory for an array
-  * @nmemb: number of element
-  * @size: size of an element
-  * Return: pointer to the memory
-  */
+ * *_memset - fills memory with a byte
+ * @q: memory
+ * @d: char
+ * @m: number
+ * Return: pointer
+ */
+char *_memset(char *q, char d, unsigned int m)
+{
+unsigned int p;
+
+for (p = 0; p < m; p++)
+{
+q[p] = d;
+}
+
+return (q);
+}
+
+/**
+ * *_calloc - allocates memory for an array
+ * @nmemb: number
+ * @size: size
+ * Return: pointer
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-char *b;
-int p = 0;
-int j = 0;
-
-j = nmemb * size;
-b = malloc(j);
-
-if (b == NULL)
-return (NULL);
+char *a;
 
 if (nmemb == 0 || size == 0)
 return (NULL);
 
-while (p < j)
-{
-b[p] = 0;
-p++;
-}
+a = malloc(size * nmemb);
 
-return (b);
+if (a == NULL)
+return (NULL);
+
+_memset(a, 0, nmemb * size);
+
+return (a);
 }
