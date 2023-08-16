6970f34d531752762de1aa3f5855dfed4d0ed0db
coeym <janemahia98@gmail.com>
2023-08-16 14:27:39 -0700

    program that performs simple operations

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -43,6 +43,12 @@ return (a * b);
   */
 int op_div(int a, int b)
 {
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+	
 return (a / b);
 }
 
@@ -54,5 +60,10 @@ return (a / b);
   */
 int op_mod(int a, int b)
 {
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}	
 return (a % b);
 }
